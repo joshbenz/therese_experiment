34b530920c68b5e42669fc0671b043fc1afc4148
Joshua Benz <joshua@planethull.com>
2019-03-22 22:41:39 -0700

    only need login

--- fuse/src/app/main/authentication/auth.module.ts
+++ fuse/src/app/main/authentication/auth.module.ts
@@ -2,22 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Login2Module } from './login-2/login-2.module';
-import { Register2Module } from './register-2/register-2.module';
-import { ResetPassword2Module } from './reset-password-2/reset-password-2.module';
-import { ForgotPassword2Module } from './forgot-password-2/forgot-password-2.module';
-import { MailConfirmModule } from './mail-confirm/mail-confirm.module';
-import { LockModule } from './lock/lock.module';
+
 
 @NgModule({
   imports: [
     CommonModule,
     Login2Module,
-    Register2Module,
-    ResetPassword2Module,
-    MailConfirmModule,
-    ForgotPassword2Module,
-    LockModule, 
-
   ],
   declarations: [
 ]
