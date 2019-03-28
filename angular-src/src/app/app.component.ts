bddecb493c99f2ee62162c25ced718ff6cba5352
Joshua Benz <joshua@planethull.com>
2019-03-27 23:37:22 -0700

    iterate over controls in template

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 
 
@@ -8,13 +9,36 @@ import { FormBuilder, FormControl, FormGroup, Validators, FormArray } from '@ang
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-src';
   isAddingData: boolean = false;
   dataForm: FormGroup;
+  private debounce: number = 400;
+
+  dogs = ['Myia', 'Mika', 'Ali'];
+  bowls = ['White', 'Clear', 'Red', 'Blue', 'Yellow'];
+  nVisits = 0;
+  render = false;
 
   constructor(private _formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
     this.dataForm = this.createForm();
+
+    this.dataForm.controls['nBowlsVisited'].valueChanges
+      .pipe(debounceTime(this.debounce), distinctUntilChanged())
+      .subscribe(query => {
+        this.nVisits = query;
+        //this.dataForm.controls.bowlsVisitedOrder.setValue(Array(this.nVisits));
+        const visited = this.dataForm.controls.bowlsVisitedOrder as FormArray
+
+        for(let i=0; i<this.nVisits; i++) {
+            visited.push(this._formBuilder.group({bowl: 'white'}));
+        }
+        console.log(this.dataForm.get('bowlsVisitedOrder'));
+
+      });
   }
 
   addData() { 
@@ -28,9 +52,12 @@ export class AppComponent {
       orderOfBowls: new FormControl(''),
       chickenBowl: new FormControl(''),
       nBowlsVisited: new FormControl(''),
-      bowlsVisitedOrder: new FormControl(''),
+      bowlsVisitedOrder: this._formBuilder.array([]),
       timeToChicken: new FormControl(''),
       comments: new FormControl('')
     });
   }
+
+  onSearchChange(searchValue : string ) {  
+    console.log(searchValue);}
 }
